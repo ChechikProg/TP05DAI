4bea4444e16896efb292e7cbd2eb9e1efd852638
LUCAS ILAN CHECHIK <49374691@ort>
2026-04-13 08:32:10 -0300

    Finalizado

--- pokeapi.js
+++ pokeapi.js
@@ -1,21 +1,25 @@
+const input = document.getElementById("input")
+const mensaje = document.getElementById("card")
 async function getPokemons() {
   try {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/Ditto");
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`);
     const data = await response.json();
-    console.log(data.name);
-    console.log(data.sprites.front_default);
     const array = [];
+    mensaje.style.display = "inline";
     const listaTipos = data.types
     listaTipos.forEach(element => {
        array.push(element.type.name) 
-    
     });
-    console.log(array);
-    console.log(data.height);
 
+    mensaje.innerHTML = `
+    <p>${data.name}</p>
+    <img src = ${data.sprites.front_default}></img>
+    <p>${array}</p>
+    <p>${data.height}</p>
+    <p>${data.weight}</p>
+    `
   } catch (error) {
     console.error("Hubo un problema con la petición:", error.message);
   }
 }
 
-getPokemons();
