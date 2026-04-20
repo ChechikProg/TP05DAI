4b3fd726c80d74f67ac8fc88d31b4661e2852432
LUCAS ILAN CHECHIK <49374691@ort>
2026-04-20 07:55:59 -0300

    incluimos msj de error

--- pokeapi.js
+++ pokeapi.js
@@ -1,25 +1,38 @@
 const input = document.getElementById("input")
 const mensaje = document.getElementById("card")
-async function getPokemons() {
-  try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`);
-    const data = await response.json();
-    const array = [];
-    mensaje.style.display = "inline";
-    const listaTipos = data.types
-    listaTipos.forEach(element => {
-       array.push(element.type.name) 
-    });
 
-    mensaje.innerHTML = `
+
+async function getPokemons() {
+
+
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`);
+
+
+  if (response.status === 404) {
+    mensaje.innerText = `No se encuentra!`;
+     mensaje.style.display = "inline";
+    return;
+  }
+
+
+  const data = await response.json();
+  const array = [];
+  mensaje.style.display = "inline";
+
+
+  const listaTipos = data.types;
+
+
+  listaTipos.forEach(element => {
+    array.push(element.type.name);
+  });
+
+
+  mensaje.innerHTML = `
     <p>${data.name}</p>
-    <img src = ${data.sprites.front_default}></img>
+    <img src="${data.sprites.front_default}">
     <p>${array}</p>
     <p>${data.height}</p>
     <p>${data.weight}</p>
-    `
-  } catch (error) {
-    console.error("Hubo un problema con la petición:", error.message);
-  }
+  `;
 }
-
